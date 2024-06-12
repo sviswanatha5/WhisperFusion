be40ed5765ddcb20e81180df5ea90b3d2e0e119b
sviswanatha6 <sviswanatha6@gatech.edu>
2024-06-12 17:24:19 -0400

    Removed python params

--- docker/scripts/run-whisperfusion.sh
+++ docker/scripts/run-whisperfusion.sh
@@ -10,8 +10,7 @@ cd /root/WhisperFusion
 if [ "$1" != "mistral" ]; then
   exec python3 main.py --phi \
                   --whisper_tensorrt_path /root/scratch-space/models/whisper_small_en \
-                  --phi_tensorrt_path /root/scratch-space/models/dolphin-2_6-phi-2 \
-                  --phi_tokenizer_path /root/scratch-space/models/dolphin-2_6-phi-2/tokenizer
+                  
 else
   exec python3 main.py --mistral \
                   --whisper_tensorrt_path /root/scratch-space/models/whisper_small_en \
