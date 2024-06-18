02d9c6f61f7a2e85a5e782b4fd51b893ebc64125
Jonpaco23 <jonpaco23@gmail.com>
2024-06-18 16:01:36 -0400

    added logging to trt server

--- whisper_live/trt_server.py
+++ whisper_live/trt_server.py
@@ -129,6 +129,8 @@ class TranscriptionServer:
         )
 
         self.clients[websocket] = client
+        logging.info(f"Websocket: {websocket}")
+        logging.info(f"Client_uid: {options["uid"]}")
         self.clients_start_time[websocket] = time.time()
         no_voice_activity_chunks = 0
         print()
