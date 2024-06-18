5ba210d8866a72bb141cb824ae4f656c52facb19
user2 <frankliu098@gmail.com>
2024-06-18 16:24:53 +0000

    Updated api_model.py

--- api_model.py
+++ api_model.py
@@ -1,5 +1,3 @@
-
-
 import requests
 import logging
 import time
@@ -51,7 +49,11 @@ class CustomLLMAPI:
         logging.info(f"Response text: {response.text}")
         
         if response.status_code == 200:
-            return response.json()['generated_text']
+            response_json = response.json()
+            if 'generated_text' in response_json:
+                return response_json['generated_text']
+            else:
+                return "Error: 'generated_text' key not found in the response."
         else:
             return "Error: " + response.text
 
@@ -71,7 +73,7 @@ class CustomLLMAPI:
                 continue
             
             if transcription_output["uid"] not in conversation_history:
-                conversation_history[transcription_output["uid"]] = []
+                conversation_history[transcription_output["uid"]] = ConversationHistory()
             
             prompt = transcription_output['prompt'].strip()
             logging.info(f"PROMPT: {prompt}")
@@ -80,7 +82,7 @@ class CustomLLMAPI:
                 self.eos = transcription_output["eos"]
                 start = time.time()
                 llm_response = self.process_transcription(transcription_output['prompt'])
-                logging.info(f"RESPonSe: {llm_response}")
+                logging.info(f"RESPONSE: {llm_response}")
                 self.infer_time = time.time() - start
                 test = []
                 test.append(llm_response)
@@ -95,95 +97,3 @@ class CustomLLMAPI:
                 continue
             
             self.last_prompt = prompt
-
-# import requests
-# import logging
-# import time
-# logging.basicConfig(level = logging.INFO)
-
-# class CustomLLMAPI:
-#     def __init__(self, api_url, api_key):
-#         self.api_url = api_url
-#         self.api_key = api_key
-#         self.headers = {'Authorization': 'Bearer ' + self.api_key}
-#         self.conv_id = ''
-#         self.new_conv_url = self.api_url + 'new_conversation?user_id='
-#         self.completion_url = self.api_url + 'completion'
-#         self.last_prompt = ""
-#         self.infer_time = 0
-
-#     def query(self, message):
-#         if not self.conv_id:
-#             r = requests.get(self.new_conv_url , headers=self.headers)
-#             self.conv_id = r.json()['data']['id']
-#             logging.info(r.json()['data'])
-#         post_data = {
-#             'conversation_id': self.conv_id,
-#             'messages': [{
-#                 'role': 'user',
-#                 'content': message
-#             }],
-#             'stream': False
-#         }
-#         response = requests.post(self.completion_url, json=post_data, headers=self.headers)
-#         return response.json()['data']['answer']
-    
-#     def process_transcription(self, transcription_text):
-#         try:
-#             llm_response = self.query(transcription_text)
-#             return llm_response
-#         except Exception as e:
-#             print(f"Error querying custom LLM API: {e}")
-#             return None
-
-
-#     def run(self, transcription_queue, audio_queue, llm_queue, lock):
-        
-#         conversation_history = {}
-#         while True:
-#             # transcription_text = transcription_queue.get()
-#             # logging.info(f"Q: {transcription_queue}")
-#             # if transcription_text:
-#             #     logging.info(f"TEXt: {transcription_text}")
-#             #     llm_response = self.process_transcription(transcription_text)
-#             #     logging.info(f"RESPONSE: {llm_response}")
-#             #     if llm_response:
-#             #         audio_queue.put(llm_response)
-                    
-            
-#             transcription_output = transcription_queue.get()
-#             if transcription_queue.qsize() != 0:
-#                 continue
-            
-#             if transcription_output["uid"] not in conversation_history:
-#                 conversation_history[transcription_output["uid"]] = []
-            
-#             prompt = transcription_output['prompt'].strip()
-#             logging.info(f"PROMPT: {prompt}")
-                                
-#             # if prompt is same but EOS is True, we need that to send outputs to websockets
-#             if self.last_prompt == prompt and transcription_output["eos"]:
-#                 # lock.acquire()
-#                 self.eos = transcription_output["eos"]
-#                 start = time.time()
-#                 llm_response = self.process_transcription(transcription_output['prompt'])
-#                 logging.info(f"RESPonSe: {llm_response}")
-#                 self.infer_time = time.time() - start
-#                 test = []
-#                 test.append(llm_response)
-#                 audio_queue.put({"llm_output": test, "eos": self.eos})
-#                 llm_queue.put({
-#                         "uid": transcription_output["uid"],
-#                         "llm_output": test,
-#                         "eos": self.eos,
-#                         "latency": self.infer_time
-#                     })
-#                 self.last_prompt = ""
-#                 # lock.release()
-#                 continue
-                
-#                 # conversation_history[transcription_output["uid"]].append(
-#                 #     (transcription_output['prompt'].strip(), self.last_output[0].strip())
-#                 # )
-#             self.last_prompt = prompt
-                    
