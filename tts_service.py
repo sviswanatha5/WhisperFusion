5b20a4039a52cfe2b2bf1b1e2d82213100c62532
sviswanatha5 <srikarviswanatha@gmail.com>
2024-06-24 14:49:09 -0400

    delete pipeline error handler

--- tts_service.py
+++ tts_service.py
@@ -58,7 +58,7 @@ class WhisperSpeechTTS:
                     #logging.info(f"Audio getting processed: {llm_output.strip()} .\n\n")
 
                     start = time.time()
-                    audio = self.pipe.generate(llm_output.strip(), step_callback=should_abort)
+                    audio = self.pipe.generate(llm_output.strip())
                     inference_time = time.time() - start
                     logging.info(f"[WhisperSpeech INFO:] TTS inference done in {inference_time} ms for  SENTENCE: {llm_output.strip()}.\n\n")
                     self.output_audio = audio.cpu().numpy()
