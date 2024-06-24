2c333fa7dc0f8c15d93d67eca7b8621a46216003
sviswanatha5 <srikarviswanatha@gmail.com>
2024-06-24 14:31:10 -0400

    log in tts

--- tts_service.py
+++ tts_service.py
@@ -55,6 +55,8 @@ class WhisperSpeechTTS:
             # only process if the output updated
             try:
                 if self.last_llm_response != llm_output.strip():
+                    logging.info(f"Audio getting processed: {llm_output.strip()} .\n\n")
+
                     start = time.time()
                     audio = self.pipe.generate(llm_output.strip(), step_callback=should_abort)
                     inference_time = time.time() - start
