a6fd0a168b8b3447aec1b2ab2171664e27a04a58
sviswanatha5 <srikarviswanatha@gmail.com>
2024-06-28 15:21:46 -0400

    added async

--- api_model.py
+++ api_model.py
@@ -32,7 +32,7 @@ class CustomLLMAPI:
         self.api_url = api_url
         self.last_prompt = ""
 
-    def run(self, transcription_queue, audio_queue, llm_queue, conversation_history):
+    async def run(self, transcription_queue, audio_queue, llm_queue, conversation_history):
         message_id = 0
         while True:
             transcription_output = transcription_queue.get()
@@ -74,13 +74,13 @@ class CustomLLMAPI:
                 
                 
                 try:
-                    with websockets.connect(self.api_url) as websocket:
+                    async with websockets.connect(self.api_url) as websocket:
                         
-                        websocket.send(history_prompt)
+                        await websocket.send(history_prompt)
                         
                         
                         while True:
-                            llm_response =  websocket.recv()
+                            llm_response =  await websocket.recv()
                             print(f"User {user} received: {llm_response}")
                             
                             logging.info(f"TRANSCRIPTION QUEUE SIZE: {transcription_queue.qsize()}")
