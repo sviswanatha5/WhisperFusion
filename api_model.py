41210659fa9feac11a5caaed210331fd30eb4d3a
Jonpaco23 <jonpaco23@gmail.com>
2024-07-01 16:01:55 -0400

    added threading

--- api_model.py
+++ api_model.py
@@ -3,6 +3,7 @@ import logging
 import time
 import json
 import asyncio
+import threading
 import websocket
 from queue import Queue
 from typing import List, Dict, Any
@@ -32,8 +33,78 @@ class CustomLLMAPI:
         self.api_url = api_url
         self.last_prompt = ""
 
+    def query(self, query, user, message_id, llm_queue, audio_queue, conversation_history, event):
+        start = time.time()
+        event = self.events[user]
+        total_response = ""
+        current_response = ""
+        llm_queue_feed = ""
+        llm_response = ""
+
+        try:
+            ws = websocket.WebSocket()
+                                    
+            ws.connect(self.api_url)
+            ws.send(json.dumps(query))
+            
+            logging.info(f"SUCCESSFULY SENT: {query}")
+            
+            while not event.is_set():
+            
+                llm_response =  ws.recv()
+                if not llm_response:
+                    continue
+                if llm_response == "<|user|>":
+                    self.eos = True
+                    llm_response = ""
+                    event.set()
+                self.infer_time = time.time() - start
+                llm_queue_feed += llm_response
+                if user not in llm_queue:
+                    llm_queue[user] = []
+                llm_queue[user] += [{
+                    "uid": user,
+                    "llm_output": llm_queue_feed,
+                    "eos": self.eos,
+                    "latency": self.infer_time
+                }]
+                
+                
+                if any(char in llm_response for char in ['.', '?', '!']):
+
+                    if "." in llm_response:
+                        split = llm_response.split(".")
+                        punc = "."
+                    elif "?" in llm_response:
+                        split = llm_response.split("?")
+                        punc = "?"
+                    else:
+                        split = llm_response.split("!")
+                        punc = "!"
+
+                    current_response += split[0] + punc
+                    logging.info(f"CURRENT RESPONSE: {current_response}")
+                    audio_queue.put({"message_id": message_id, "llm_output": current_response})
+                    logging.info(f"SENT TO AUDIO_QUEUE: {current_response}")
+                    total_response += current_response
+
+                    current_response = ""
+                else:
+                    current_response += llm_response
+                    
+                logging.info(f"RESPONSE: {llm_response}")
+                    
+        except Exception as e:
+            logging.info(f"Exception: {e}")
+            pass
+                
+        conversation_history[user].add_to_history("assistant", total_response)
+        self.last_prompt = ""  # Reset last prompt after processing
+        self.eos = False  # Reset eos after processing
+
     def run(self, transcription_queue, audio_queue, llm_queue, conversation_history):
         message_id = 0
+        self.events = {}
         while True:
             transcription_output = transcription_queue.get()
             if transcription_queue.qsize() != 0:
@@ -42,22 +113,15 @@ class CustomLLMAPI:
             prompt = transcription_output['prompt'].strip()
             user = transcription_output['uid']
 
-            #logging.info(f"PROMPT: {prompt}, EOS: {transcription_output["eos"]}")
+            if transcription_output and user in self.events:
+                self.events[user].set()
             
             if user not in conversation_history:
                 conversation_history[user] = ConversationHistory()
                 
             if self.last_prompt == prompt and transcription_output["eos"]:
                 self.eos = False
-                start = time.time()
                 conversation_history[user].add_to_history("user", prompt)
-                
-                total_response = ""
-
-                current_response = ""
-                llm_queue_feed = ""
-                
-                llm_response = ""
                 
                 messages = [{"speaker": "user", "message": prompt}]
                 formatted_prompt = messages[-1]['message']
@@ -66,150 +130,13 @@ class CustomLLMAPI:
                 query = [{"role": "user", "content": history_prompt}]
                 logging.info(f"Sending request to: {self.api_url}")
                 
-                 
-                try:
-                    
-                    ws = websocket.WebSocket()
-                        
-                    #logging.info(f"WEBSOCKET: {ws}")
-                    
-                    ws.connect(self.api_url)
-                    ws.send(json.dumps(query))
-                    
-                    logging.info(f"SUCCESSFULY SENT: {query}")
-                    
-                    while True:
-                    
-                        llm_response =  ws.recv()
-                        if not llm_response:
-                            continue
-                        
-                        logging.info(f"TRANSCRIPTION QUEUE SIZE: {transcription_queue.qsize()}")
-                        if transcription_queue.qsize() > 0:
-                            transcription_output = transcription_queue.get()
-                            test = transcription_output["prompt"]
-                            logging.info(f"Transciprtion queue contents: {test}")
-                            if transcription_output["prompt"] != "":
-                                break
-                        
-                        if llm_response == "<|user|>":
-                            self.eos = True
-                            llm_response = ""
-                        self.infer_time = time.time() - start
-                        llm_queue_feed += llm_response
-                        if user not in llm_queue:
-                            llm_queue[user] = []
-                        llm_queue[user] += [{
-                            "uid": user,
-                            "llm_output": llm_queue_feed,
-                            "eos": self.eos,
-                            "latency": self.infer_time
-                        }]
-                        
-                        
-                        if any(char in llm_response for char in ['.', '?', '!']):
+                if user in self.events:
+                    self.events[user].set()
+                self.events[user] = threading.Event()
+                thread = threading.Thread(self.query, args=(query, user, message_id, llm_queue, audio_queue, conversation_history))
+                thread.start()
 
-                            if "." in llm_response:
-                                split = llm_response.split(".")
-                                punc = "."
-                            elif "?" in llm_response:
-                                split = llm_response.split("?")
-                                punc = "?"
-                            else:
-                                split = llm_response.split("!")
-                                punc = "!"
-
-                            
-
-                            current_response += split[0] + punc
-                            logging.info(f"CURRENT RESPONSE: {current_response}")
-                            audio_queue.put({"message_id": message_id, "llm_output": current_response})
-                            logging.info(f"SENT TO AUDIO_QUEUE: {current_response}")
-                            total_response += current_response
-
-                            current_response = ""
-                        else:
-                            current_response += llm_response
-                            
-                        logging.info(f"RESPONSE: {llm_response}")
-                    
-                    
-                    
-                    
-                    #file.write(response + "\n")
-                        #file.flush()  
-                except Exception as e:
-                    logging.info(f"Exception: {e}")
-                    #print(f"User {user_id} connection closed, retrying...")
-                    #await asyncio.sleep(2)  
-
-                # with requests.get(self.api_url, params=params, stream=True) as response:
-                #     logging.info(f"Response status code: {response.status_code}")
-                #     if response.status_code == 200:
-                #         for chunk in response.iter_content(1024):
-                #             logging.info(f"TRANSCRIPTION QUEUE SIZE: {transcription_queue.qsize()}")
-                #             if transcription_queue.qsize() > 0:
-                #                 transcription_output = transcription_queue.get()
-                #                 test = transcription_output["prompt"]
-                #                 logging.info(f"Transciprtion queue contents: {test}")
-                #                 if transcription_output["prompt"] != "":
-                #                     break
-                #             llm_response = chunk.decode('utf-8')
-                #             if llm_response == "AI":
-                #                 break
-                #             if llm_response == "<|user|>":
-                #                 self.eos = True
-                #                 llm_response = ""
-                #             self.infer_time = time.time() - start
-                #             llm_queue_feed += llm_response
-                #             if user not in llm_queue:
-                #                 llm_queue[user] = []
-                #             llm_queue[user] += [{
-                #                 "uid": user,
-                #                 "llm_output": llm_queue_feed,
-                #                 "eos": self.eos,
-                #                 "latency": self.infer_time
-                #             }]
-                            
-                            
-                #             if any(char in llm_response for char in ['.', '?', '!']):
-
-                #                 if "." in llm_response:
-                #                     split = llm_response.split(".")
-                #                     punc = "."
-                #                 elif "?" in llm_response:
-                #                     split = llm_response.split("?")
-                #                     punc = "?"
-                #                 else:
-                #                     split = llm_response.split("!")
-                #                     punc = "!"
-
-                                
-
-                #                 current_response += split[0] + punc
-                #                 logging.info(f"CURRENT RESPONSE: {current_response}")
-                #                 audio_queue.put({"message_id": message_id, "llm_output": current_response})
-                #                 logging.info(f"SENT TO AUDIO_QUEUE: {current_response}")
-                #                 total_response += current_response
-
-                #                 current_response = ""
-                #             else:
-                #                 current_response += llm_response
-                                
-                #             logging.info(f"RESPONSE: {llm_response}")
-                            
-                #             logging.info(f"API INFERENCE TIME: {self.infer_time}")
-                #             #logging.info(f"Response chunk: {chunk.decode('utf-8')}")
-                #     else:
-                #         return "Error: " + response.text
-                
-                conversation_history[user].add_to_history("assistant", total_response)
-                #audio_queue.put({"message_id": message_id, "llm_output": llm_response, "eos": self.eos})
-                self.last_prompt = ""  # Reset last prompt after processing
-                self.eos = False  # Reset eos after processing
                 message_id += 1
-                continue
             
             self.last_prompt = prompt
             
-            #test git
