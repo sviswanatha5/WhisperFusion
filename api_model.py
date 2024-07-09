456a6115921e47e06e9c09938c610084dae2a612
sviswanatha5 <srikarviswanatha@gmail.com>
2024-07-09 14:29:03 -0400

    log

--- api_model.py
+++ api_model.py
@@ -169,7 +169,9 @@ class CustomLLMAPI:
             #     self.events[user].set()
             
             websocket.ping()
-+           logging.info(f"Websocket: {websocket.id}")
+            logging.info(f"Websocket: {websocket.id}")
+            
+            
             
             if user not in self.conversation_history:
                 self.conversation_history[user] = ConversationHistory()
