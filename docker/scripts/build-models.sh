110e6ecf3fe00baa2601cd648fae77b0e205e3ad
Jonpaco23 <jonpaco23@gmail.com>
2024-07-10 16:08:29 -0400

    changed more scripts

--- docker/scripts/build-models.sh
+++ docker/scripts/build-models.sh
@@ -2,7 +2,7 @@
 
 test -f /etc/shinit_v2 && source /etc/shinit_v2
 
-if [ ! -d "/root/scratch-space/models/whisper_small_en" ] || [ -z "$(ls -A /root/scratch-space/models/whisper_small_en)" ]; then
+if [ ! -d "/root/scratch-space/models/whisper_medium" ] || [ -z "$(ls -A /root/scratch-space/models/whisper_medium)" ]; then
     echo "whisper_small_en directory does not exist or is empty. Running build-whisper.sh..."
     ./build-whisper.sh
 else
