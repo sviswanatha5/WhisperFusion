b719a6a27c6648aa8baa07ba77fc312a0b0cbb94
Jonpaco23 <jonpaco23@gmail.com>
2024-07-25 15:07:45 -0400

    reverted previous chnage

--- examples/chatbot/html/js/main.js
+++ examples/chatbot/html/js/main.js
@@ -297,9 +297,7 @@ function initWebSocket() {
         console.log("message_id: " + message_id);
         console.log("current_message_id: " + current_message_id);
         console.log("Blacklist: " + blacklist);
-        if (current_message_id > message_id) {
-            current_message_id = message_id
-        } else if (message_id < current_message_id) {
+        if (message_id < current_message_id) {
             return
         }
         let audioBuffer = audioContext_tts.createBuffer(1, float32Array.length, tts_sampling_rate);
