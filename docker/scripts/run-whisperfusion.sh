51def9cd84c6b196a628707b0f1c74a187df4206
Jonpaco23 <jonpaco23@gmail.com>
2024-08-01 15:39:39 -0400

    changed run

--- docker/scripts/run-whisperfusion.sh
+++ docker/scripts/run-whisperfusion.sh
@@ -7,16 +7,7 @@ cd /root/scratch-space/
 ./build-models.sh
 
 cd /root/WhisperFusion
-if [ "$1" != "mistral" ]; then
-  exec python3 api_main.py --phi \
-                  --whisper_tensorrt_path /root/scratch-space/models/whisper_medium \
-                  --api_url ws://12.1.52.180:8001/ws
-                  
-                  
-                  
-else
-  exec python3 main.py --mistral \
-                  --whisper_tensorrt_path /root/scratch-space/models/whisper_small_en \
-                  --mistral_tensorrt_path /root/scratch-space/models/mistral \
-                  --mistral_tokenizer_path teknium/OpenHermes-2.5-Mistral-7B
-fi
+exec python3 api_main.py --phi \
+                --whisper_tensorrt_path /root/scratch-space/models/whisper_medium \
+                --api_url ws://12.1.52.180:8001/ws
+
